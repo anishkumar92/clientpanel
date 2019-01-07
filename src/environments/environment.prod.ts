c90034db36076bb255668916f3071ca5e78b358b
anish kumar <anishkumar92@gmail.com>
2019-01-07 16:53:22 +0530

    get and add clients set up with firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyBt_es3ekeB_cbTHBRio30LGF4zzDsH52M",
+    authDomain: "clientpanel-4b860.firebaseapp.com",
+    databaseURL: "https://clientpanel-4b860.firebaseio.com",
+    projectId: "clientpanel-4b860",
+    storageBucket: "clientpanel-4b860.appspot.com",
+    messagingSenderId: "318058452895"
+ 
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyBt_es3ekeB_cbTHBRio30LGF4zzDsH52M",
+    authDomain: "clientpanel-4b860.firebaseapp.com",
+    databaseURL: "https://clientpanel-4b860.firebaseio.com",
+    projectId: "clientpanel-4b860",
+    storageBucket: "clientpanel-4b860.appspot.com",
+    messagingSenderId: "318058452895"
+ 
+  }
 };
 
 /*
